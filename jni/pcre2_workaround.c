b162f1242a7f88035288b2ef74202da7b55f77bf
eraselk <eraselk@proton.me>
2024-06-27 18:13:52 +0800

    Fix error

--- jni/pcre2_workaround.c
+++ jni/pcre2_workaround.c
@@ -1,4 +1,4 @@
 /* Workaround pcre2_chartables.c symlink to pcre2_chartables.c.dist failing on Windows NDK if Cygwin git used,
    and NDK not directly accepting a .c.dist file in LOCAL_SRC_FILES list. */
 
-#include "pcre/include/pcre2.h"
+#include "pcre/src/pcre2_chartables.c.dist"
