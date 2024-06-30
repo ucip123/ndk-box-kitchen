69644eaffeec346af11b560f65d177e007ee39bd
eraselk <eraselk@proton.me>
2024-06-30 12:07:35 +0800

    [TEST] [POLLY] Use --polly-parallel

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,6 +1,6 @@
 APP_ABI := armeabi-v7a arm64-v8a
 APP_PLATFORM := android-35
-APP_CFLAGS := -Wall -fomit-frame-pointer -Ofast -mllvm -polly -fuse-ld=lld -flto
+APP_CFLAGS := -Wall -fomit-frame-pointer -Ofast -mllvm -polly -mllvm -polly-parallel -fuse-ld=lld -flto
 APP_LDFLAGS := -flto
 
 ifeq ($(OS),Windows_NT)
