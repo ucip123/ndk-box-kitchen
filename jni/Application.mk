b0971c4471409f158d7b9c0c3cedfab27b144b76
Your Name <you@example.com>
2024-07-01 09:09:10 +0800

    Updaate

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,6 +1,6 @@
 APP_ABI := armeabi-v7a arm64-v8a
 APP_PLATFORM := android-35
-APP_CFLAGS := -Wall -fomit-frame-pointer -Ofast -mllvm -polly -flto -fuse-ld=lld
+APP_CFLAGS := -Wall -fomit-frame-pointer -Ofast -mllvm -polly -flto -fuse-ld=gold
 APP_LDFLAGS := -flto -Wl,--gc-sections -Wl,--as-needed
 
 ifeq ($(OS),Windows_NT)
